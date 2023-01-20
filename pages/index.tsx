73bc2dcee5349488c47de13be3a8c4ed695b8c7c
Strike24 <ostapenko.andrey03@gmail.com>
2023-01-20 14:41:49 +0200

    Homepage

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+      <button
+        type="button"
+        className="bg-violet-800 rounded-full font-sans p-6 w-96 text-slate-100 text-lg font-bold shadow-xl animate-bounce"
+      >
+        Sign In
+      </button>
+    </div>
+  );
+}
