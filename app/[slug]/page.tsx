a23d1cd5ddb908359bc3e1d8230cde8f97be70d2
ProStrike2448 <ostapenko.andrey03@gmail.com>
2023-01-24 17:54:00 +0200

    UI Changes

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -22,17 +22,19 @@ export default function User() {
 
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="backdrop-blur bg-white/25 rounded-lg shadow-xl w-96 h-96 p-12 flex justify-center gap-8">
+      <div className="backdrop-blur bg-white/25 rounded-lg shadow-xl w-80 h-96 p-6 flex justify-center gap-8">
         <div className="flex justify-center h-20 gap-8">
-            <Image
-              src="https://pure.au.dk/portal/files/126962919/Nickolaj_Kristensen_PURE.jpg"
-              alt="Profile Picture"
-              width={80}
-              height={80}
-              priority={true}
-              className="object-cover rounded-full overflow-hidden"
-            />
-          <h1 className="font-semibold text-white text-2xl self-center">{"Nickolaj"}</h1>
+          <Image
+            src="https://pure.au.dk/portal/files/126962919/Nickolaj_Kristensen_PURE.jpg"
+            alt="Profile Picture"
+            width={80}
+            height={80}
+            priority={true}
+            className="object-cover rounded-full overflow-hidden"
+          />
+          <h1 className="font-semibold text-white text-2xl self-center">
+            {"Nickolaj"}
+          </h1>
         </div>
       </div>
     </div>
